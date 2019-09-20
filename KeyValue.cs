e414f86048d9d7bdc4bcf44264f2d721cd87dc78
Ibrahim <ibrahim.toure2019@outlook.com>
2019-09-20 08:20:33 -0400

    Separated class files

---
+++ KeyValue.cs
@@ -0,0 +1,15 @@
+﻿namespace KeyValueStore
+{
+    struct KeyValue
+    {
+        public readonly string key;
+        public readonly object value;
+
+        public KeyValue(string key, object value)
+        {
+            this.key = key;
+            this.value = value;
+        }
+
+    }
+}
